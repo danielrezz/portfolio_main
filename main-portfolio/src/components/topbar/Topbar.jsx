8798af1cc85b318b6e45bc87fb7b762202b8bcbf
Daniel Reza <daniel.reza1007@gmail.com>
2021-05-17 22:02:19 -0500

    first commit

---
+++ main-portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">danielreza</a>
+                </div>
+                <div className="right">
+                    right side
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
